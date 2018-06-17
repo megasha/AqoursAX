b14c4228df5a1eac3c4a3983a6b1c5f19ad09408
Jimmy N <jimnguyen123@gmail.com>
2018-06-17 01:43:53 -0700

    Added Kananrail/Yousoroad highlights

---
+++ Assets/Scripts/KananrailHighlightManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KananrailHighlightManager : MonoBehaviour {
+    public GameObject leftPit;
+    public GameObject rightPit;
+    public GameObject leftOrchestra;
+    public GameObject rightOrchestra;
+    public GameObject leftLodge;
+    public GameObject rightLodge;
+
+    // Use this for initialization
+    void Start () {
+        leftPit.GetComponent<Renderer>().enabled = false;
+        rightPit.GetComponent<Renderer>().enabled = false;
+        leftOrchestra.GetComponent<Renderer>().enabled = false;
+        rightOrchestra.GetComponent<Renderer>().enabled = false;
+        leftLodge.GetComponent<Renderer>().enabled = false;
+        rightLodge.GetComponent<Renderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            leftPit.GetComponent<Renderer>().enabled = !leftPit.GetComponent<Renderer>().enabled;
+            rightPit.GetComponent<Renderer>().enabled = !rightPit.GetComponent<Renderer>().enabled;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            leftOrchestra.GetComponent<Renderer>().enabled = !leftOrchestra.GetComponent<Renderer>().enabled;
+            rightOrchestra.GetComponent<Renderer>().enabled = !rightOrchestra.GetComponent<Renderer>().enabled;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            leftLodge.GetComponent<Renderer>().enabled = !leftLodge.GetComponent<Renderer>().enabled;
+            rightLodge.GetComponent<Renderer>().enabled = !rightLodge.GetComponent<Renderer>().enabled;
+        }
+    }
+}
---
+++ Assets/Scripts/YousororoadHightlightManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YousororoadHightlightManager : MonoBehaviour {
+    public GameObject pit;
+    public GameObject orchestra;
+    public GameObject lodge;
+
+
+	// Use this for initialization
+	void Start () {
+        pit.GetComponent<Renderer>().enabled = false;
+        orchestra.GetComponent<Renderer>().enabled = false;
+        lodge.GetComponent<Renderer>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            pit.GetComponent<Renderer>().enabled = !pit.GetComponent<Renderer>().enabled;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            orchestra.GetComponent<Renderer>().enabled = !orchestra.GetComponent<Renderer>().enabled;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            lodge.GetComponent<Renderer>().enabled = !lodge.GetComponent<Renderer>().enabled;
+        }
+    }
+}
