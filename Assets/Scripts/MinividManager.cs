7d830ef8d1b6772580b6ccab0f9abbae1ddfd0ab
Jimmy N <jimnguyen123@gmail.com>
2018-06-17 13:18:38 -0700

    Added minivid manager

---
+++ Assets/Scripts/MinividManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinividManager : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        GetComponent<RawImage>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            GetComponent<RawImage>().enabled = !GetComponent<RawImage>().enabled;
+        }
+    }
+}
